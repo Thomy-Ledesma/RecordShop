cbea087c5cf4ba96b7b1fe1c2e82d82ae101a1ff
Thomás Ledesma <98626681+Thomy-Ledesma@users.noreply.github.com>
2024-08-07 19:58:18 -0300

    First Commit

---
+++ Domain/Entities/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Band { get; set; }
+        public string genre { get; set; }
+        public int Amount { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ Domain/Entities/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Sale
+    {
+        public int Id { get; set; }
+        public List<Album> Products { get; set; }
+        public int CustomerId { get; set; }
+        public bool State {  get; set; }
+        public float Total {  get; set; }
+    }
+}

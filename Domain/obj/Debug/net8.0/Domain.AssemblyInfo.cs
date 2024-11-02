d87be5b3e99e6ddf71cd4a443a0e4f4088fbf676
Thomás Ledesma <98626681+Thomy-Ledesma@users.noreply.github.com>
2024-11-02 01:42:12 -0300

    Authentication roles update

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d7f714885f84df4a64bb2279c4378fceec84cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4260a36d98bf8b9912bd2dc7dcc022fd6c29a96f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RecordShop/obj/Debug/net8.0/RecordShop.AssemblyInfo.cs
+++ RecordShop/obj/Debug/net8.0/RecordShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecordShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d7f714885f84df4a64bb2279c4378fceec84cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4260a36d98bf8b9912bd2dc7dcc022fd6c29a96f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecordShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecordShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
